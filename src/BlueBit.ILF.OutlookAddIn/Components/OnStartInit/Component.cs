c077d72a2ecc74e9415a445d0088a94b8f44e89f
Tomasz <tomasz.orynski@gmail.com>
2017-07-11 17:33:37 +0200

    Hack for Outlook errors when first enumerate calendar folders

---
+++ src/BlueBit.ILF.OutlookAddIn/Components/OnStartInit/Component.cs
@@ -0,0 +1,31 @@
+﻿using BlueBit.ILF.OutlookAddIn.Common.Extensions;
+using BlueBit.ILF.OutlookAddIn.Common.Utils;
+using System;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace BlueBit.ILF.OutlookAddIn.Components.OnStartInit
+{
+    [Obsolete("Hack for Outlook errors when first enumerate calendar folders")]
+    public class Component :
+        ISelfRegisteredComponent
+    {
+        public void Initialize(Outlook.Application app)
+        {
+            Func<Outlook.Folder> getRootFolder = app
+                .GetNamespace("MAPI")
+                .GetDefaultFolder(Outlook.OlDefaultFolders.olFolderCalendar)
+                .As<Outlook.Folder>;
+
+            using (var foldersSource = new FoldersSource(
+                getRootFolder(),
+                s => true,
+                s => true
+                ))
+                foldersSource.EnumFolders((fld, sel) => { });
+        }
+
+        public void Execute()
+        {
+        }
+    }
+}
